95fd3233a9f7f01207baa65211aed3ec2c4e3c7d
Lucas Leow <leowjianhoong@gmail.com>
2023-06-08 02:07:37 +0800

    Completed setting up tailwind with vite

---
+++ TailWindCSS/Tailwind3EsstlTraining/tailwind-proj/vite.config.js
@@ -0,0 +1,8 @@
+export default {
+    root: 'src', // root directory of index.html
+    publicDir: 'public', // files that will directly push to server 
+    outDir: '../docs', // output directory 
+    server: { // server command for live preview server, open window when server run
+        open: true,
+    }
+}
